91fe3f0b56a7075b8b78953e0276ec61ae0c2bde
himanshu <aryanhimanshu50@gmail.com>
2019-10-01 15:55:38 +0530

    my name is himanshu shekhar

---
+++ fullstackjavascript/app.js
@@ -0,0 +1,10 @@
+
+//output method
+window.alert 2+2;
+window.confirm('conferm here');
+window.prompt('promt here');
+//window.document.write('hello');
+//window.document.body.innerHTML=('inner html');
+//window.document.body.innerText=('inner text');
+
+//this is for window object....
